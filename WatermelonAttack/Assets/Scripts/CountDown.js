f52d3b3911e23cbf3b10cb67266eb05a164c900a
Mason Barry <creativemessss@gmail.com>
2015-03-20 12:03:13 -0500

    Optimized Countdown Script
    GameObject.Find was rinning 7 times every frame. It now runs at the
    start of the game only twice.

--- WatermelonAttack/Assets/Scripts/CountDown.js
+++ WatermelonAttack/Assets/Scripts/CountDown.js
@@ -5,11 +5,21 @@ public var number3 : Texture;
 public var number4 : Texture;
 public var number5 : Texture;
 
+public var countdownObj : GameObject;
+public var imageCountdownObj : GameObject;
+
 function Start () {
  
 	GetComponent("OVRPlayerController").Acceleration = 0.0;
 	GetComponent("OVRPlayerController").RotationAmount = 0.0;
 	GameObject.Find("Countdown").GetComponent(AudioSource).Play();
+	
+	//Storing this as a variable prevents the game from
+	//running the "GameObject.Find" function every frame 
+	//in the Update function.
+	countdownObj = GameObject.Find("Countdown");
+	imageCountdownObj = GameObject.Find("ImageCountdown");
+	
 }
  
 function Update () {
@@ -17,35 +27,35 @@ function Update () {
         timeLeft -= Time.deltaTime;
         
         if ( timeLeft > 4 && timeLeft <= 5 ) {
-        	GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number5;
+        	imageCountdownObj.GetComponent(UnityEngine.UI.RawImage).texture = number5;
         }
         
         if ( timeLeft > 3 && timeLeft <= 4 ) {
-			GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number4; 	
+			imageCountdownObj.GetComponent(UnityEngine.UI.RawImage).texture = number4; 	
         }
         
         if ( timeLeft > 2 && timeLeft <= 3 ) {
-			GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number3;
+			imageCountdownObj.GetComponent(UnityEngine.UI.RawImage).texture = number3;
         }
         
         if ( timeLeft > 1 && timeLeft <= 2 ) {
-			GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number2;
+			imageCountdownObj.GetComponent(UnityEngine.UI.RawImage).texture = number2;
         }
         
         if ( timeLeft > 0 && timeLeft <= 1 ) {
-        	GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number1;
+        	imageCountdownObj.GetComponent(UnityEngine.UI.RawImage).texture = number1;
         }
         
         if ( timeLeft < 0 ) {
             
-            GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).enabled = false;
+            imageCountdownObj.GetComponent(UnityEngine.UI.RawImage).enabled = false;
 			GetComponent("OVRPlayerController").Acceleration = 0.4;
 			GetComponent("OVRPlayerController").RotationAmount = 1.5;
 			
         }
         
         if ( timeLeft < -1 ) {
-        	GameObject.Find("Countdown").SetActive(false);
+        	countdownObj.SetActive(false);
         	GetComponent("CountDown").enabled = false;
         }
   
