ab5e8ee0a945e6d55f36833c39b877a36704adf1
Mason Barry <creativemessss@gmail.com>
2015-03-20 14:53:17 -0500

    Added clouds under the level
    Added clouds to the scene under the level to give it a more appealing
    atmosphere.

---
+++ WatermelonAttack/Assets/Scripts/clouds.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var animationSpeed : float;
+var offset : float;
+
+function Start () {
+
+}
+
+function Update () {
+	
+	offset += Time.deltaTime;
+	GetComponent.<Renderer>().material.mainTextureOffset = Vector2(0, offset * animationSpeed);
+}
