5bfb8997b09f89a32a2ed5dbc94ce1f58ca4f41f
Alberto Punter <dragost11@gmail.com>
2015-03-20 11:25:07 +0100

    Pause Script Updated

--- WatermelonAttack/Assets/Scripts/Pause.js
+++ WatermelonAttack/Assets/Scripts/Pause.js
@@ -3,12 +3,17 @@
 public var btnPause : KeyCode = KeyCode.P;
 
 function Start () {
-
+	GetComponent.<AudioListener>().pause = false;
 }
 
 function Update () {
 
 	if (Input.GetKeyDown(btnPause)){
+		if(GetComponent.<AudioListener>().pause){ 
+			GetComponent.<AudioListener>().pause = false;
+		} else {
+			GetComponent.<AudioListener>().pause = true;
+		}
 		Time.timeScale = 1.0-Time.timeScale;
 		Time.fixedDeltaTime = 0.02 * Time.timeScale;
 	}
