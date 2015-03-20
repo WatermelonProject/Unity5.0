9975f3cdb1e778af658a4e90781b6ed101958802
Alberto Punter <dragost11@gmail.com>
2015-03-20 15:16:30 +0100

    Countdown with images

--- WatermelonAttack/Assets/Scripts/CountDown.js
+++ WatermelonAttack/Assets/Scripts/CountDown.js
@@ -1,12 +1,13 @@
-﻿
+﻿public var timeLeft : float = 5.0;
+public var number1 : Texture;
+public var number2 : Texture;
+public var number3 : Texture;
+public var number4 : Texture;
+public var number5 : Texture;
 
-public var timeLeft : float = 5.0;
-
- 
 function Start () {
  
 	GetComponent("OVRGamepadController").enabled = false;
-	
 	GetComponent("OVRPlayerController").enabled = false;
 
 }
@@ -14,11 +15,36 @@ function Start () {
 function Update () {
  
         timeLeft -= Time.deltaTime;
+        
+        if ( timeLeft > 4 && timeLeft <= 5 ) {
+        	GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number5;
+        }
+        
+        if ( timeLeft > 3 && timeLeft <= 4 ) {
+			GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number4; 	
+        }
+        
+        if ( timeLeft > 2 && timeLeft <= 3 ) {
+			GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number3;
+        }
+        
+        if ( timeLeft > 1 && timeLeft <= 2 ) {
+			GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number2;
+        }
+        
+        if ( timeLeft > 0 && timeLeft <= 1 ) {
+        	GameObject.Find("ImageCountdown").GetComponent(UnityEngine.UI.RawImage).texture = number1;
+        }
+        
+        
+        
         if ( timeLeft < 0 ) {
-               
-		GetComponent("OVRGamepadController").enabled = true;
-		
-		GetComponent("OVRPlayerController").enabled = true;
+        
+            GameObject.Find("Countdown").active = false;
+            
+			GetComponent("OVRGamepadController").enabled = true;
+			GetComponent("OVRPlayerController").enabled = true;
+			
 		
         }  
              
