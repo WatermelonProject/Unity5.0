19e2e94200afb4047854c59c2859d79a1a962402
Mason Barry <creativemessss@gmail.com>
2015-03-20 12:18:04 -0500

    Experimental 3rd person character added
    This is just a placeholder for the character, but it gives us something
    to work with.

---
+++ WatermelonAttack/Assets/Test Character/Scripts/blobController.js
@@ -0,0 +1,87 @@
+﻿#pragma strict
+
+var anim : Animation;
+
+//var walkSpeed : float;
+//var topWalkSpeed : float;
+//
+//var rotationSpeed : float;
+//
+//var jumpForce : float;
+//
+private var scale : Vector3;
+//private var velocityLog : Vector3;
+//
+//private var lastPosition : Vector3;
+//private var currentWalkSpeed : float;
+//
+//var topWalkSpeedStart : float;
+
+function Start () {
+	scale = transform.localScale;
+//	topWalkSpeedStart = topWalkSpeed;
+}
+
+function Update () {
+	
+//	currentWalkSpeed = Vector3.Distance(lastPosition, transform.position);
+	
+	if (Input.GetKeyDown(KeyCode.W)) {
+		anim.Play("Walk");
+	}
+	
+	if (Input.GetKeyUp(KeyCode.W)) {
+		anim.Stop("Walk");
+		transform.localScale = scale;
+	}
+	
+	if (Input.GetKeyDown(KeyCode.S)) {
+		anim.Play("Walk");
+	}
+	
+	if (Input.GetKeyUp(KeyCode.S)) {
+		anim.Stop("Walk");
+		transform.localScale = scale;
+	}
+	
+//  Experimental walking script apart from the OVRPlayer Controller
+	
+//	if (Input.GetKey(KeyCode.LeftShift)) {
+//		topWalkSpeed = topWalkSpeedStart + 0.05;
+//	} else {
+//		topWalkSpeed = topWalkSpeedStart;
+//	}
+//	
+//	if (Input.GetKeyDown(KeyCode.Space)) {
+//		GetComponent.<Rigidbody>().AddRelativeForce(transform.up * jumpForce);
+//	}
+//	
+//	if (Input.GetKey(KeyCode.W)) {
+//	
+//		if (currentWalkSpeed < topWalkSpeed) {
+//			velocityLog = GetComponent.<Rigidbody>().velocity;
+//			GetComponent.<Rigidbody>().velocity = (transform.forward * walkSpeed) + velocityLog;
+//		}
+//		
+//	}
+//	
+//	if (Input.GetKey(KeyCode.S)) {
+//		
+//		if (currentWalkSpeed < topWalkSpeed) {
+//			velocityLog = GetComponent.<Rigidbody>().velocity;
+//			GetComponent.<Rigidbody>().velocity = (transform.forward * -walkSpeed) + velocityLog;
+//		}
+//		
+//	}
+//	
+//	if (Input.GetKey(KeyCode.A)) {
+//		transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+//	}
+//	
+//	if (Input.GetKey(KeyCode.D)) {
+//		transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+//	}
+//	
+//	lastPosition = transform.position;
+	
+}
---
+++ WatermelonAttack/Assets/Test Character/Scripts/camFollow.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var target : Transform;
+
+var distanceFromTarget : float;
+
+var distanceFromGround : float;
+
+function Start () {
+	
+}
+
+function Update () {
+	
+	transform.position.x = target.position.x;
+	transform.position.y = target.position.y + distanceFromGround;
+	transform.position.z = target.position.z + distanceFromTarget;
+	
+}
