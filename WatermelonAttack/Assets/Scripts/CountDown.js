7961860086ac47fd980b05e12bb066d522adf958
David Perez <dpm_garrapa@hotmail.com>
2015-03-20 12:20:24 +0100

    CountDown Script

---
+++ WatermelonAttack/Assets/Scripts/CountDown.js
@@ -0,0 +1,25 @@
+﻿
+
+public var timeLeft : float = 5.0;
+
+ 
+function Start () {
+ 
+	GetComponent("OVRGamepadController").enabled = false;
+	
+	GetComponent("OVRPlayerController").enabled = false;
+
+}
+ 
+function Update () {
+ 
+        timeLeft -= Time.deltaTime;
+        if ( timeLeft < 0 ) {
+               
+		GetComponent("OVRGamepadController").enabled = true;
+		
+		GetComponent("OVRPlayerController").enabled = true;
+		
+        }  
+             
+}
