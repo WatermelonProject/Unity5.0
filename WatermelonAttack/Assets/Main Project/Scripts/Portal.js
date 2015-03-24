a7b30618410f71677899fae46c8b20a2fafaff90
Mason Barry <creativemessss@gmail.com>
2015-03-24 18:24:42 -0500

    New portal script to replace finish script.
    The portal script allows you to set the name of the level you want it to
    take the player. It also has the option to set it as the end of the
    game.

---
+++ WatermelonAttack/Assets/Main Project/Scripts/Portal.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+private var finished : boolean = false;
+public var resetKey : KeyCode = KeyCode.Return; 
+public var finishedText : Canvas;
+public var finishedTextR : Canvas;
+
+var OVRCameraController : Transform;
+
+var nextLevelName : String;
+
+var endOfGame : boolean;
+
+function Start () {
+//	finishedText.enabled = false;
+//	finishedTextR.enabled = false;
+	Time.timeScale = 1.0;
+}
+
+function Update () {
+	if(Input.GetKeyDown(resetKey) && finished) {
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
+
+function OnCollisionEnter (collision : Collision) {
+
+	if (collision.gameObject.tag=="player") {
+		
+		if (endOfGame) {
+			
+			finished = true;
+			finishedText.enabled = true;
+			finishedTextR.enabled = true;
+			Time.timeScale = 0.0;
+			
+		} else {
+			
+			OVRCameraController.GetComponent(fadeInOut).levelToLoad = nextLevelName;
+			OVRCameraController.GetComponent(fadeInOut).changeLevelFade = true;
+			
+		}
+	}
+
+}
